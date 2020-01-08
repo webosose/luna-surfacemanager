676fdb617c20e409e1eea2a1bdc515cae4d114f5
Jungi Byun <jungi.byun@lge.com>
2020-01-08 09:19:05 +0900

    Fix missing assignment operator and copy constructor
    Change-Id: I0478e0378c1ca3c64fcf9da35abfd13df4e2433b

--- modules/weboscompositor/webossurfaceitem.cpp
+++ modules/weboscompositor/webossurfaceitem.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2019 LG Electronics, Inc.
+// Copyright (c) 2013-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1111,6 +1111,9 @@ public:
     {
         delete shmTex;
     }
+
+    BufferAttacher(const BufferAttacher&) = delete;
+    BufferAttacher &operator=(const BufferAttacher&) = delete;
 
     void attach(const QWaylandBufferRef &ref) Q_DECL_OVERRIDE
     {
