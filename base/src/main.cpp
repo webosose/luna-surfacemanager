e370efd78a111982026e7b172ae651a5fa48b0f8
Jaeyoon Jung <jaeyoon.jung@lge.com>
2021-03-26 17:46:37 +0900

    base: Emit lsm-ready only if autoStart is set
    Change-Id: I8610ade7e00e4f2981bbcff7697f0fbb96bf7e54

--- base/src/main.cpp
+++ base/src/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020 LG Electronics, Inc.
+// Copyright (c) 2017-2021 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -235,7 +235,8 @@ int main(int argc, char *argv[])
     compositor->postInit();
 
 #ifdef UPSTART_SIGNALING
-    compositor->emitLsmReady();
+    if (compositor->autoStart())
+        compositor->emitLsmReady();
 #endif
 
     // Create $XDG_RUNTIME_DIR/surface-manager.windowcount
